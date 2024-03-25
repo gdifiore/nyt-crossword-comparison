2e0dd327357d59908ece36d569057ae684bc5156
Gabriel DiFiore <difioregabe@gmail.com>
2024-03-25 12:33:29 -0400

    added test chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PrintDB from './components/printData';
 import TimeInput from './components/TimeInput';
-//import Chart from './components/Chart'
-import chartXkcd from 'chart.xkcd';
-import { Bar } from "chart.xkcd-react"
+import { BarH } from "react-roughviz";
 
 const App = () => {
   return (
@@ -11,22 +9,13 @@ const App = () => {
       <h1>Hello, React!</h1>
       <TimeInput />
       <PrintDB />
-      <Bar
-        config={{
-          title: 'github stars VS patron number', // optional
-          // xLabel: '', // optional
-          // yLabel: '', // optional
-          data: {
-            labels: ['github stars', 'patrons'],
-            datasets: [{
-              data: [100, 2],
-            }],
-          },
-          options: { // optional
-            yTickCount: 2,
-          },
+      <div class="viz"></div>
+      <BarH
+        data= {{
+          labels: ['a', 'b'],
+          values: [10, 20]
         }}
-      />
+    />
     </div>
   );
 };
