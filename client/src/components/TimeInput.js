f7289f7bf7ba3108c0011330667a176cc9defbad
Gabriel DiFiore <difioregabe@gmail.com>
2024-05-09 16:02:52 -0400

    Update TimeInput.js

--- client/src/components/TimeInput.js
+++ client/src/components/TimeInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function TimeInput({ onTimeInput }) {
   const [inputValue, setInputValue] = useState('');
